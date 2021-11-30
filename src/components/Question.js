e8d9c9289fce815a99a712b8e8ed98070d4c3e7b
Sergio99778 <sergio4brandon@gmail.com>
2021-11-30 13:29:20 -0600

    Adding Error component to Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,18 +1,36 @@
 import React, { Fragment, useState } from 'react';
 
+//Components
+import Error from './Error';
+
 const Question = () => {
   //Budget quantity
   const [budget, setBudget] = useState(0);
+  //Error
+  const [error, setError] = useState(false);
 
   //handleChange
   const handleChange = (e) => {
     setBudget(parseInt(e.target.value));
   };
 
+  //handleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //validate
+    if (budget < 1 || isNaN(budget)) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+    }
+  };
+
   return (
     <Fragment>
       <h2>Define your budget</h2>
-      <form action="">
+      {error ? <Error message="The budget is not valid" /> : null}
+      <form onSubmit={handleSubmit} action="">
         <input onChange={handleChange} type="number" className="u-full-width" placeholder="Budget" value={budget} />
         <button className="button-primary u-full-width" type="submit">
           Define !
